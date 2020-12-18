450fee39b88669c3b910235cadaeb4e7833dbe70
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-18 16:51:56 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,29 @@
 fun main(){
-    rectangle(5,10)
+    rectangle(10,7)
     println()
     println()
-    rectangle(2,1)
+    rectangle(5,5)
+    triangle(10,5)
 }
 
 fun rectangle(rows: Int,columns: Int){
-    for(i in 1..columns){
+
+    for(j in 1..rows) {
+
+        for (i in 1..columns) {
+            print("*")
+        }
+
+        println()
+    }
+}
+
+fun triangle(a: Int, h: Int){
+    for (i in 1..a) {
         print("*")
     }
+}
+
+fun triangle(a: Int){
+
 }
